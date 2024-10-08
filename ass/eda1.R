023f258841d4151bc2cfab30765abe117ab946ec
Andre Wang <z5362811@ad.unsw.edu.au>
2024-10-08 19:05:44 +1100

    more personal exploration tue 8 oct

--- ass/eda1.R
+++ ass/eda1.R
@@ -15,7 +15,9 @@ nrow(claims_data) # claim_id is not a primary key in claims_data
 
 claims_data %>% filter(claim_id == "claim_e379e560-6324-46ae-92a0-a4cfc6bb3525")
 claims_data %>% filter(claim_id == "claim_0eb34739-5fd4-4d91-b45e-ea4ca077ffd7")
+claims_data %>% filter(claim_id == "claim_02e2cb40-65e9-4c6a-b25b-799d06c0f7a4")
 claims_data %>% filter(claim_id == "claim_2a6f8e5d-a334-4684-ac81-003174cae85d")
+claims_data %>% filter(exposure_id == "exposure_4464f014-04bd-47b4-8cc5-1fec8e92b31e") # one exposure can have different claims.
 claims_data %>% group_by(claim_id) %>% summarise(n=n()) %>% arrange(desc(n))
 
 
@@ -26,8 +28,8 @@ as_tibble(claims_data[duplicated(claims_data),])
 # money on a claim id
 t <- claims_data %>% filter(total_claim_amount >0) %>% select(total_claim_amount, claim_id, exposure_id)
 length(unique(t$claim_id, t$exposure_id))
-dupeNonZeroClaims <- tibble(t[duplicated(t[,2:3]),]) %>% arrange(claim_id, exposure_id)
-# tenures between datasets should be identical.
+dupeNonZeroClaims <- tibble(t[duplicated(t[,2:3]),]) %>% arrange(claim_id, exposure_id) # 'split' claims can have their amounts distributed across entries.
+# tenures between datasets should be identical...
 # tenure + nb_policy_first_inception_date (earneddata) should be the claim_start_date or UW date...
 # what is quoting?
 
@@ -36,3 +38,9 @@ tenures <- claims_data %>% select(tenure, exposure_id)
 tenures2 <- earned_data %>% select(tenure, exposure_id)
   
 t <- merge(x=tenures, y=earned_data, by=c("exposure_id", "tenure"), all.x=TRUE)
+
+# why are there multiple exposure entries in earned_data? One per tenure month.
+t1 <- earned_data %>% filter(duplicated(earned_data$exposure_id)) %>% arrange(exposure_id) #note that the first entry will not be shown (usually tenure 0)
+View(earned_data %>% filter(exposure_id == "exposure_000e4bc1-8623-43a5-a47f-3685d8d084be"))
+# reduce the rows - select max tenure and first uw_date
+exposures <- earned_data %>% group_by(exposure_id) %>% summarise(max_tenure = max(tenure), first_uw_date = min(UW_Date), total_earned_units = sum(earned_units), across(-c(UW_Date, tenure, earned_units), first))
--- ass/sevmodel1.R
+++ ass/sevmodel1.R
@@ -1,14 +1,79 @@
+library(tidyverse)
+library(ggplot2)
+library(lubridate)
+library(MASS) #stepAIC and glm.nb()
+#### Data setup ####
+claims_data <- read_csv("UNSW_claims_data.csv")
+
+#Skipping first column which is just numbers
+earned_data <- read_csv("UNSW_earned_data_adjusted_Sep27.csv")[, -1]
+
+# price_output <- read_csv("Data/Sample_price_output_file.csv")
+
+# Changing claim status and condition category to factors
+claims_data <- claims_data %>%
+  mutate(
+    claim_status = as.factor(claim_status),
+    condition_category = as.factor(condition_category)
+  )
+#Changing variables to factors
+earned_data <- earned_data %>%
+  mutate(across(
+    c(pet_gender, pet_de_sexed_age, nb_address_type_adj, nb_suburb, nb_postcode,
+      nb_state, pet_age_years, nb_breed_type, nb_breed_trait),
+    as.factor
+  ))
+#Number of claims per exposure id, need to divide this by the exposure
+exposure_id_claims_count <- claims_data %>%
+  group_by(exposure_id) %>%
+  summarize(unique_claim_count = n_distinct(claim_id))
+#Cost per exposure. Would need to do offsetting by claimnb or something, since that impacts an exposure's claim cost.
+exposure_id_claims_cost <- claims_data %>%
+  group_by(exposure_id) %>%
+  summarize(sum_claims_cost = sum(total_claim_amount)) # kinda wanted to sum costs for a single claim, then costs for an exposure, but done in one sum.
+
+# For severity, might be better just to look at claims rather than each exposure.
+claim_costs <- claims_data %>% group_by(claim_id, exposure_id) %>% summarise(sum_claims_cost = sum(total_claim_amount))
+
+# Condense data to talk about each exposure. !!! Grouping OK????
+exposures <- earned_data %>% group_by(exposure_id) %>% summarise(max_tenure = max(tenure), first_uw_date = min(UW_Date), total_earned_units = sum(earned_units), across(-c(UW_Date, tenure, earned_units), first))
+sev_data1 <- merge(claim_costs, exposures, by = "exposure_id")
+sev_data <- sev_data1 %>% filter(sum_claims_cost != 0)
+rm(sev_data1)# 57 entries have 0 claim cost:
+  #check inception and uw first are different.
+# 500 policies where first uw month is not the same as inception date. 
+# sev_data %>% select(nb_policy_first_inception_date, first_uw_date) %>% filter(month(nb_policy_first_inception_date) != month(first_uw_date))
+
+#### Doing Severity ####
+set.seed(888)  
+train_indices <- sample(1:nrow(claim_costs), size = 0.7 * nrow(claim_costs)) 
+sev_data_train = sev_data[train_indices, ]
 # Create the real density plot using ggplot
-ggplot(filter(freMTPL2full[train_indices, ], ClaimNb > 0), aes(x = ClaimTotal)) +
-  geom_density(color = "darkblue", linewidth = 0.8) +
+ggplot(claim_costs, aes(x = sum_claims_cost)) +
+  geom_density(color = "darkblue") +
   labs(title = "Empirical Density of Claim Amounts",
        x = "Claim Total",
        y = "Density") +
-  xlim(0, 10000) + 
+#  xlim(0, 10000) + 
   theme_minimal()
+summary(claim_costs)
 
 # build gamma and inverse gaussian with log link.
+sevGamma_full <- glm(sum_claims_cost ~ pet_gender + pet_de_sexed + pet_age_months +
+                       nb_contribution + nb_excess + nb_address_type_adj + nb_state + nb_contribution_excess +
+                       owner_age_years + nb_number_of_breeds + nb_average_breed_size +  nb_breed_type + nb_breed_trait + is_multi_pet_plan ,
+                     family = Gamma(link = "log"), 
+                     data = sev_data_train)
+summary(sevGamma_full) #ass
 # Subset selection
+sev_none <- glm(sum_claims_cost ~ 1,
+                family = Gamma(link = "log"), 
+                data = sev_data_train)
+sevGamma_back<-stepAIC(sevGamma_full,
+                  direction = "backward",
+                  k = 2,
+                  scope = list(upper = sevGamma_full, lower = sev_none)
+)
 # Randomised quantile Residuals (difficult)
 # qq plot (on residuals)
 #chisq. Not appropriate?
